54704220053dafe2dcf02fb08cc4ef93352d0013
Ashikuzzaman_Moon <ashikuzzamanmoon31@gmail.com>
2023-05-13 00:17:35 +0600

    module 67

--- index.js
+++ index.js
@@ -14,50 +14,74 @@ const uri = "mongodb+srv://ashikuzzaman:mIT6sTe3cCDLpFvX@cluster0.zjtbp3d.mongod
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    const database = client.db("usersDB");
-    const userCollection = database.collection("users");
+        const database = client.db("usersDB");
+        const userCollection = database.collection("users");
 
-    app.get('/users', async(req, res)=> {
-        const cursor = userCollection.find()
-        const result = await cursor.toArray();
-        res.send(result);
-    })
+        app.get('/users', async (req, res) => {
+            const cursor = userCollection.find()
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
-    app.post('/users', async(req, res)=> {
-        const user = req.body;
-        console.log('new user', user);
-        const result = await userCollection.insertOne(user);
-        res.send(result);
-    })
+        app.get('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const user = await userCollection.findOne(query);
+            res.send(user);
+        })
 
-    app.delete('/users/:id', async(req, res)=> {
-        const id = req.params.id;
-        console.log('please delete from database', id);
-        const query = { _id: new ObjectId(id)}
-        const result = await userCollection.deleteOne(query);
-        res.send(result);
-    })
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log('new user', user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.put('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const user = req.body;
+            console.log(id, user);
+             
+            const filter = { _id: new ObjectId(id) }
+            const options = { upsert: true };
+            const updatedUser = {
+                $set: {
+                    name: user.name,
+                    email: user.email
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedUser, options);
+            res.send(result);
+        })
+
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log('please delete from database', id);
+            const query = { _id: new ObjectId(id) }
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -66,6 +90,6 @@ app.get('/', (req, res) => {
     res.send('SIMPLE CRUD IS RUNNING')
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`SIMPLE CRUD is running on port, ${port}`)
 })
